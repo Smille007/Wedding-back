60bcbb5762204103fb6610e1376444e7a600dc1c
Smille007 <smille007@gmai.com>
2024-05-31 16:42:42 -0400

    route changed

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-// DEPENDENCIES
 const express = require("express");
 const cors = require("cors");
+const app = express();
 const rsvpController = require('./controllers/rsvpController');
 
-// CONFIGURATION
-require("dotenv").config();
-const app = express();
-
-const corsOptions = {
-    origin: 'http://localhost:3001', // Replace with your frontend URL
-    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-};
-
-// MIDDLEWARE
-app.use(cors(corsOptions)); // Apply custom CORS options
+app.use(cors());
 app.use(express.json());
-
-// ROUTES
+// Mount the rsvpController middleware under the /rsvp path
 app.use('/rsvp', rsvpController);
+
+
 
 app.get("/", (req, res) => {
     res.json("Welcome to Daryna's wedding website");
