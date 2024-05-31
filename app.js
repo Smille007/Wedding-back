cdc78ef2fb2233e2084219a37d574d5ca83a8297
Smille007 <smille007@gmai.com>
2024-05-31 11:57:31 -0400

    commit

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const rsvpController = require('./controllers/rsvpController');
 require("dotenv").config();
 const app = express();
 
+const corsOptions = {
+    origin: 'http://localhost:3001', // Replace with your frontend URL
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
+
 // MIDDLEWARE
-app.use(cors());
+app.use(cors(corsOptions)); // Apply custom CORS options
 app.use(express.json());
 
 // ROUTES
