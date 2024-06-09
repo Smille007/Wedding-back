eea9011b3cc7516de654baee127c3e6b7a3a2f55
Smille007 <smille007@gmai.com>
2024-06-09 14:06:36 -0400

    connection db

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const app = require("./app");
+// const app = require("./app");
+// require('dotenv').config();
+
+
+
+// const PORT = process.env.PORT || 4005;
+
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}`);
+// });
 require('dotenv').config();
+const { Pool } = require('pg');
 
-
-// CONFIGURATION
-const PORT = process.env.PORT || 4005;
-
-// LISTEN
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
+
+// Test the database connection
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the database successfully!');
+  }
+  release(); // Release the client back to the pool
+});
+
+module.exports = pool;
+
