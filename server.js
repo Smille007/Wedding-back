510ecdbbd5a1f7da46dd6965b7abb902e4043bfe
Smille007 <smille007@gmai.com>
2024-06-09 15:58:55 -0400

    changes

--- server.js
+++ server.js
@@ -9,14 +9,32 @@
 // app.listen(PORT, () => {
 //   console.log(`Listening on port ${PORT}`);
 // });
+// server.js
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4005; // Use the PORT environment variable or fallback to 4005
+const rsvpController = require('./controllers/rsvpController');
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Mount the rsvpController middleware under the /rsvp path
+app.use('/rsvp', rsvpController);
+
+// Route for the homepage
+app.get("/", (req, res) => {
+    res.json("Welcome to Daryna's wedding website");
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
 
 
 
