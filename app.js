08f23b1483293603167a745c5daeb0aa4cf037af
Smille007 <smille007@gmai.com>
2024-07-05 17:43:36 -0400

    took away rsvp in link

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const db = require('./db/dbConfig'); // Import database configuration
 app.use(cors());
 app.use(express.json());
 // Mount the rsvpController middleware under the /rsvp path
-app.use('/rsvp', rsvpController);
+app.use('/', rsvpController);
 //ddd
 
 // Test the database connection
